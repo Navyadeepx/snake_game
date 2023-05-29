9022c5b4c14737194ee7df235a9cdcd1b3c8bf5d
Navyadeepx <112097544+Navyadeepx@users.noreply.github.com>
2023-05-30 04:52:03 +0530

    simplyfication

--- snake.py
+++ snake.py
@@ -1,9 +1,8 @@
 from tkinter import *
 import random
-
 root = Tk()
 #window
-root.geometry("610x600")
+root.geometry("800x800")
 root.resizable(False,False)
 #title
 root.title("snake game indev")
@@ -22,21 +21,32 @@ def mf():
 		Grid.rowconfigure(mainframe, i, weight=1)
 	for j in range(0,32):
 		Label(mainframe,text="",bg='black').grid(row=j,column=j,sticky="NEWS")
-	return(mainframe)
+	return mainframe
+mainframe=LabelFrame(root,bg='black')
+mainframe.grid(row=1, column=0,rowspan=5, columnspan=5,sticky="EWNS",padx=5,pady=5)
+for i in range(0,32):
+	Grid.columnconfigure(mainframe, i, weight=1)
+	Grid.rowconfigure(mainframe, i, weight=1)
+for j in range(0,32):
+	Label(mainframe,text="",bg='black').grid(row=j,column=j,sticky="NEWS")
 #food pos
-def fpos():
-	mf()
-	x=random.randrange(0,32)
-	y=random.randrange(0,32)
-	food=Label(mf(),text="",bg='red')
-	food.grid(row=x,column=y,sticky="NEWS")
-#play button
-playb=Button(root, text="play",command=fpos)
-playb.grid(row=0, column=2,sticky='ew')
+x=random.randrange(0,32)
+y=random.randrange(0,32)
+food=Label(mainframe,text="",bg='red')
+food.grid(row=x,column=y,sticky="NEWS")
+cord=(x,y)
+#snake pos
+x=random.randrange(0,32)
+y=random.randrange(0,32)
+if cord[0]==x and cord[1]==y:
+	x=(x+1)//1
+	y=(y+1)//1
+snake=Label(mainframe,text="",bg='green')
+snake.grid(row=x,column=y,sticky="NEWS")
 #high score
-hs=Label(root, text='high score:')
-hs.grid(row=0,column=0,sticky='ew')
+hs=Label(root, text='high score:',padx=10)
+hs.grid(row=0,column=0,sticky='w')
 #score
 cs=Label(root, text='current score:')
-cs.grid(row=0,column=4,sticky='ew')
+cs.grid(row=0,column=4,sticky='e',padx=10)
 root.mainloop()
