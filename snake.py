c0d1da8fda0ab9e46139fef3575e2d98808bbb39
Navyadeepx <112097544+Navyadeepx@users.noreply.github.com>
2023-05-30 04:53:47 +0530

    simplification, increased window size

--- snake.py
+++ snake.py
@@ -6,38 +6,26 @@ root.geometry("800x800")
 root.resizable(False,False)
 #title
 root.title("snake game indev")
-Grid.columnconfigure(root, 0, weight=1)
-Grid.columnconfigure(root, 1, weight=1)
-Grid.columnconfigure(root, 2, weight=1)
-Grid.columnconfigure(root, 3, weight=1)
-Grid.columnconfigure(root, 4, weight=1)
+for i in range(5):
+	Grid.columnconfigure(root, i, weight=1)
 Grid.rowconfigure(root,1, weight=1)
 #main frame
-def mf():
-	mainframe=LabelFrame(root,bg='black')
-	mainframe.grid(row=1, column=0,rowspan=5, columnspan=5,sticky="EWNS")
-	for i in range(0,32):
-		Grid.columnconfigure(mainframe, i, weight=1)
-		Grid.rowconfigure(mainframe, i, weight=1)
-	for j in range(0,32):
-		Label(mainframe,text="",bg='black').grid(row=j,column=j,sticky="NEWS")
-	return mainframe
 mainframe=LabelFrame(root,bg='black')
 mainframe.grid(row=1, column=0,rowspan=5, columnspan=5,sticky="EWNS",padx=5,pady=5)
-for i in range(0,32):
+for i in range(32):
 	Grid.columnconfigure(mainframe, i, weight=1)
 	Grid.rowconfigure(mainframe, i, weight=1)
-for j in range(0,32):
+for j in range(32):
 	Label(mainframe,text="",bg='black').grid(row=j,column=j,sticky="NEWS")
 #food pos
-x=random.randrange(0,32)
-y=random.randrange(0,32)
+x=random.randrange(32)
+y=random.randrange(32)
 food=Label(mainframe,text="",bg='red')
 food.grid(row=x,column=y,sticky="NEWS")
 cord=(x,y)
 #snake pos
-x=random.randrange(0,32)
-y=random.randrange(0,32)
+x=random.randrange(32)
+y=random.randrange(32)
 if cord[0]==x and cord[1]==y:
 	x=(x+1)//1
 	y=(y+1)//1
